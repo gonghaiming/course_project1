881af959d8cbc1deec338f4b6a03a0d9cc24ca26
gonghaiming <364276231@qq.com>
2016-03-01 15:31:14 +0800

    Initial Commit

---
+++ project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by MR_gong on 16/3/1.
+//  Copyright © 2016年 gong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
